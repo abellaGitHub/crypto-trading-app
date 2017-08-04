900263f0e2e06813a0d395ff4575812502f91424
abella <bella.aleksander@gmail.com>
2017-08-04 16:58:23 +0200

    Continue to connect with Bitfinex WebSocket

--- imports/exchanges/bitfinex.js
+++ imports/exchanges/bitfinex.js
@@ -9,25 +9,46 @@ export const BitfinexData = new Mongo.Collection('bitfinex');
 if(Meteor.isServer) {
 	Meteor.startup(function() {
 
-		const wss = new WebSocket('wss://api.bitfinex.com/ws');
+		const wss = new WebSocket('wss://api.bitfinex.com/ws/2');
 
-		wss.on('message', function(msg) {
-			console.log('Message:')
-			console.log(msg);
-		});
+		Meteor.wrapAsync(wss.on('message', function(msg) {
+			var res = JSON.parse(msg);
+	
+			if(res.toString() === '[object Object]') {
+				if(res.event === 'subscribed') {
+					console.log(res);
+				} else if(res.event === 'info') {
+					console.log(res);
+				}
+			} else {
+				var dataArray = res[1];
 
+				if(typeof dataArray !== 'string') {
+					var bid = dataArray[0];
+					var ask = dataArray[2];
+					var last = dataArray[6];
+					var mid = (bid + ask) / 2;
+
+					console.log('Last:', last, 'Bid:', bid, 'Ask:', ask, 'Mid:', mid);
+				} else {
+					console.log('It is just a heartbeat ...');
+				}
+			}
+		}));
+ 
 		wss.on('open', function() {
 			console.log('Connected to Bitfinex WebSocket');
+			// wss.send(JSON.stringify({event: 'ping'}));
+
+			let subReq = {
+				event: 'subscribe',
+				channel: 'ticker',
+				symbol: 'tBTCUSD'
+			};
+
+			wss.send(JSON.stringify(subReq));
 		});
 
-		/*wss.on('pong', function(msg) {
-			console.log('Pong:')
-			console.log(msg);
-		});
-
-		wss.on('open', function() {
-			wss.send('message', {'event': 'ping'});
-		});*/
 		/*
 		Meteor.setInterval(function() {
 			HTTP.get('https://api.bitfinex.com/v1/pubticker/btcusd', function(error, response) {
